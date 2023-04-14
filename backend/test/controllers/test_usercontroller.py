8de17284bdbbf7841aabc2a0e04a17bd949f5bb2
fridavbg <fperssontech@gmail.com>
2023-04-14 14:41:27 +0200

    Five unit tests for usercontroller get_user_by_email

--- backend/test/controllers/test_usercontroller.py
+++ backend/test/controllers/test_usercontroller.py
@@ -2,7 +2,6 @@ import pytest
 import unittest.mock as mock
 
 from src.controllers.usercontroller import UserController
-from src.util.dao import DAO
 
 
 @pytest.fixture
@@ -15,21 +14,30 @@ def mocked_user_controller(mocked_dao):
     # Create a mock UserController object with the mocked DAO object
     user_controller = UserController(dao=mocked_dao)
 
-    # Create a mock user object with a unique email address
-    mock_user = {
-        '_id': '12345',
-        'email': 'jane.doe@gmail.com',
-        'firstName': 'Jane',
-        'lastName': 'Doe'
-    }
+    # Create a mock user object with a duplicate email addresses
+    mock_users = [
+        {
+            '_id': '12345',
+            'email': 'jane.doe@gmail.com',
+            'firstName': 'Jane',
+            'lastName': 'Doe'
+        },
+        {
+            '_id': '67890',
+            'email': 'jane.doe@gmail.com',
+            'firstName': 'John',
+            'lastName': 'Doe'
+        }
+    ]
 
     # Set the return value of the UserController get_user_by_email function to the mock user object
-    user_controller.get_user_by_email = mock.MagicMock(return_value=mock_user)
+    user_controller.get_user_by_email = mock.MagicMock(
+        return_value=mock_users[0])
 
     return user_controller
 
 
-def test_get_user_by_email_one_match(mocked_user_controller):
+def test_get_user_by_valid_email(mocked_user_controller):
     # Call the get_user_by_email method on the mocked user controller
     result = mocked_user_controller.get_user_by_email('jane.doe@gmail.com')
 
@@ -42,7 +50,20 @@ def test_get_user_by_email_one_match(mocked_user_controller):
     }
 
 
-def test_get_user_by_email_no_match(mocked_user_controller):
+def test_get_user_by_email_duplicate(mocked_user_controller):
+    # Call the get_user_by_email method on the mocked user controller
+    result = mocked_user_controller.get_user_by_email('jane.doe@gmail.com')
+
+    # Assert that the method returned the expected user object
+    assert result == {
+        '_id': '12345',
+        'email': 'jane.doe@gmail.com',
+        'firstName': 'Jane',
+        'lastName': 'Doe'
+    }
+
+
+def test_get_user_by_not_existing_email(mocked_user_controller):
     # Call the get_user_by_email method on the mocked user controller
     result = mocked_user_controller.get_user_by_email('joe.doe@gmail.com')
 
@@ -50,9 +71,25 @@ def test_get_user_by_email_no_match(mocked_user_controller):
     assert result == {}
 
 
-def test_get_user_by_email_valuerror():
-    # Call the get_user_by_email method on the mocked user controller
-    result = mocked_user_controller.get_user_by_email('Hej')
+def test_get_user_by_invalid_email(mocked_user_controller):
+    # Call the get_user_by_email method on the mocked user controller with invalid email addresses
+    with pytest.raises(ValueError):
+        result = mocked_user_controller.get_user_by_email('Hej')
 
-    # Assert that the method returned the expected user object
-    assert result == 'ValueError'
+    with pytest.raises(ValueError):
+        result = mocked_user_controller.get_user_by_email('example.com')
+
+    with pytest.raises(ValueError):
+        result = mocked_user_controller.get_user_by_email('user@')
+
+    with pytest.raises(ValueError):
+        result = mocked_user_controller.get_user_by_email('@example.com')
+
+
+def test_get_user_by_email_exception(mocked_user_controller):
+    # Set the get_user_by_email method of the mocked user controller to raise an exception
+    mocked_user_controller.get_user_by_email.side_effect = Exception()
+
+    # Call the get_user_by_email method on the mocked user controller
+    with pytest.raises(Exception):
+        result = mocked_user_controller.get_user_by_email('jane.doe@gmail.com')
