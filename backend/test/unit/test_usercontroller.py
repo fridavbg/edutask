57877b12c8d0d6edd2b4a640bc40b4ee2cacd131
emcofah <emfh21@student.bth.se>
2023-04-15 09:23:46 +0200

    code optimization

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -3,7 +3,7 @@ import unittest.mock as mock
 
 from src.controllers.usercontroller import UserController
 
-
+# Setup for test
 @pytest.fixture
 def sut(email: str, res):
     mockeduser = mock.MagicMock()
@@ -12,65 +12,39 @@ def sut(email: str, res):
     return mockedsut
 
 
+obj = {'id': 1, 'firstName': 'John',
+       'lastName': 'Doe', 'email': 'test@test.com'}
+
 # test get_user_by_email, valid email
+# test 1 expected = return value of valid email
+# test 2 expected = return first value in array if multiple values
+# test 3 expected = return None if array is empty
 @pytest.mark.unit
 @pytest.mark.parametrize('email, res, expected',
                          [
-                             ('test@test.com',
-                              [{'id': 1, 'firstName': 'John',
-                                  'lastName': 'Doe', 'email': 'test@test.com'}],
-                              {'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'email': 'test@test.com'}),
+                             ('test@test.com', [obj], obj),
+                             ('test@test.com', [obj, obj, obj], obj),
+                             ('test@test.com', [], None)
                          ]
                          )
 def test_get_user_by_valid_email(sut, email, expected):
     res = sut.get_user_by_email(email=email)
     assert res == expected
 
-# test get_user_by_email, duplicate valid email
-
-
-@pytest.mark.unit
-@pytest.mark.parametrize('email, res, expected',
-                         [
-                             ('test@test.com',
-                              [{'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'email': 'test@test.com'},
-                               {'id': 2, 'firstName': 'Jane',
-                                   'lastName': 'Doe', 'email': 'test@test.com'},
-                               {'id': 3, 'firstName': 'Bob', 'lastName': 'Smith', 'email': 'test@test.com'}],
-                              {'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'email': 'test@test.com'}),
-                         ]
-                         )
-def test_get_user_by_duplicate_valid_email(sut, email, expected):
-    res = sut.get_user_by_email(email=email)
-    assert res == expected
-
-# test get_user_by_email, not existing email
-
-
-@pytest.mark.unit
-@pytest.mark.parametrize('email, res, expected',
-                         [
-                             ('test4@test.com', [], None)
-                         ]
-                         )
-def test_get_user_by_not_existing_email(sut, email, expected):
-    res = sut.get_user_by_email(email=email)
-    assert res == expected
-
 
 # test get_user_by_email, unvalid email
+# ValueError expected
 @pytest.mark.unit
 @pytest.mark.parametrize('email, res, expected',
-                         [('test_unvalid', ['test@test.com'], 'test@test.com'),
-                          ('', ['test@test.com'], 'test@test.com')])
+                         [('test_unvalid', ['test@test.com'], 'test@test.com')])
 def test_get_user_by_unvalid_email(sut, email, expected):
     with pytest.raises(ValueError):
         res = sut.get_user_by_email(email=email)
         assert res == expected
 
+
 # test get_user_by_email, Exception
-
-
+# Exception expected
 @pytest.mark.unit
 @pytest.mark.parametrize('email, res, expected',
                          [('test@test.com', Exception, 'test@test.com')])
