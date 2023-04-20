6077d0ec27cb2235d6ff1c87e2045dffd493f9da
emcofah <emfh21@student.bth.se>
2023-04-20 14:46:08 +0200

    added objects to variables

--- backend/test/util/test_dao.py
+++ backend/test/util/test_dao.py
@@ -7,6 +7,19 @@ from pymongo.errors import WriteError
 from src.util.dao import DAO
 
 
+valid_data = {"firstName": "john", "lastName": "doe",
+              "email": "test@test.com", "bool": True}
+
+invalid_data = {"firstName": "john", "lastName": 378,
+                "email": "test@test.com", "bool": True}
+
+missing_data = {"firstName": "john", "lastName": 378,
+                "email": "test@test.com"}
+
+duplicate_email = {"firstName": "jane", "lastName": "doe",
+                   "email": "test@test.com", "bool": True}
+
+
 class TestDatabase:
     @pytest.fixture
     def sut(self):
@@ -50,30 +63,26 @@ class TestDatabase:
 
     @pytest.mark.staging
     def test_create_valid_data(self, sut):
-        user = sut.create({"firstName": "john", "lastName": "doe",
-                           "email": "test@test.com", "bool": True})
-        assert type(user) == dict
-        assert user["name"] == "john"
+
+        return_data = sut.create(valid_data)
+        assert type(return_data) == dict
+        assert return_data["firstName"] == "john"
 
     @pytest.mark.parametrize("data", [
-        ({"firstName": "john", "lastName": 378,
-         "email": "test@test.com", "bool": True}),
-        ({"firstName": "john", "email": "test@test.com"})
+        (invalid_data)
+    ])
+    def test_create_invalid_data(self, sut, data):
+        with pytest.raises(WriteError):
+            sut.create(data)
+
+    @pytest.mark.parametrize("data", [
+        (missing_data)
     ])
     def test_create_missing_properties(self, sut, data):
         with pytest.raises(WriteError):
             sut.create(data)
 
     def test_create_none_unique(self, sut):
-        sut.create({"firstName": "test", "lastName": "doe",
-                   "email": "test@test.com", "bool": True})
+        sut.create(valid_data)
         with pytest.raises(WriteError):
-            sut.create({"firstName": "john", "lastName": "doe",
-                       "email": "test@test.com", "bool": True})
-
-    def test_create_invalid_unique(self, sut):
-        sut.create({"firstName": "test", "lastName": "doe",
-                   "email": "test@test.com", "bool": True})
-        with pytest.raises(WriteError):
-            sut.create({"firstName": "john", "lastName": "doe",
-                       "email": "test@test.com", "bool": False})
+            sut.create(duplicate_email)
